0f323968253fc5bd719ddcdf9722c03ec65a98e5
Hesham Almatary <hesham.almatary@data61.csiro.au>
2017-06-12 14:34:40 +1000

    RISCV32: Update console driver and start.S with priv-1.10
    Hello World and Ticker work on Spike

--- c/src/lib/libbsp/riscv32/riscv_generic/console/console-io.c
+++ c/src/lib/libbsp/riscv32/riscv_generic/console/console-io.c
@@ -31,14 +31,9 @@
 #include <assert.h>
 #include <stdio.h>
 
-#if __riscv_xlen == 64                                                        
 # define TOHOST_CMD(dev, cmd, payload) \                                      
   (((uint64_t)(dev) << 56) | ((uint64_t)(cmd) << 48) | (uint64_t)(payload))   
-#else                                                                         
-# define TOHOST_CMD(dev, cmd, payload) ({ \                                   
-  if ((dev) || (cmd)) __builtin_trap(); \                                     
-  (payload); })                                                               
-#endif                                                                        
+
 #define FROMHOST_DEV(fromhost_value) ((uint64_t)(fromhost_value) >> 56)       
 #define FROMHOST_CMD(fromhost_value) ((uint64_t)(fromhost_value) << 8 >> 56)  
 #define FROMHOST_DATA(fromhost_value) ((uint64_t)(fromhost_value) << 16 >> 16)
@@ -103,41 +98,14 @@ static void do_tohost_fromhost(uintptr_t dev, uintptr_t cmd, uintptr_t data)
     }
 }
 
-void htif_syscall(uintptr_t arg)
-{
-  do_tohost_fromhost(0, 0, arg);
-}
-
 void htif_console_putchar(uint8_t ch)
 {
     __set_tohost(1, 1, ch);
 }
 
-long frontend_syscall(long n, uint64_t a0, uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4, uint64_t a5, uint64_t a6)
-{
-  static volatile uint64_t magic_mem[8];
-
-  magic_mem[0] = n;
-  magic_mem[1] = a0;
-  magic_mem[2] = a1;
-  magic_mem[3] = a2;
-  magic_mem[4] = a3;
-  magic_mem[5] = a4;
-  magic_mem[6] = a5;
-  magic_mem[7] = a6;
-
-  htif_syscall((uintptr_t)magic_mem);
-
-  long ret = magic_mem[0];
-
-  return ret;
-}
-
 long handle_trap(uint32_t cause, uint32_t epc, uint64_t regs[32])
 {
-  /*FIXME: Currently only handling outchar traps */
-  htif_console_putchar(regs[10]);
-
+  /* TODO: handle traps correctly, and move this function to a right place */
   return epc+4;
 }
 
@@ -150,7 +118,7 @@ void console_initialize_hardware(void)
 
 static void outbyte_console(char ch)
 {
-  frontend_syscall(1, ch, 0, 0, 0, 0, 0, 0);
+  htif_console_putchar(ch);
   return 0;
 }
 
